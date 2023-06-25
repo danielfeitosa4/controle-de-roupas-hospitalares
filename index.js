75dc9e59a578fc64bd33ba88e438b7f13304cf9e
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-06-24 21:44:52 -0300

    arquivo index do projeto

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const conn = require('./db/conn')
+
+const app = express()
+
+// Models
+const Usuario = require('./models/Usuario')
+
+// Routers
+const usuarioRoutes = require('./routers/usuarioRoutes')
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/usuarios', usuarioRoutes)
+
+conn
+  // .sync()
+  .sync({ force: true })
+  .then(() => {
+    app.listen(4000)
+  })
+  .catch((err) => console.log(err))
