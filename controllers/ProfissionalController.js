eb84471ef983bb964faff8e34d0aa7b36a09a279
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-07-09 22:25:01 -0300

    criacao do profissionalController

---
+++ controllers/ProfissionalController.js
@@ -0,0 +1,17 @@
+const Profissional = require('../models/Profissional')
+
+module.exports = class ProfissionalController {
+
+  static cadastroDeEntrega(req, res) {
+
+    res.render('profissional/form')
+
+  }
+
+  static async inserirCadastro(req, res) {
+
+    
+
+  }
+
+}
